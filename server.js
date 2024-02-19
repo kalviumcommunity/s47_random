304c5f8e6643f7046ebb7a3e2f4a945acf96904f
Gouranshvaishnavji <gouransh.v@kalvium.community>
2024-02-19 08:57:10 +0530

    This commit is for the module 7 ASAP, I have connected my code to database and added user credentials to it.

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const app = express();
-const port = process.env.PUBLIC_PORT || 3000 ;
-
-app.get ('/ping', (request, resolve)=>{
-    //showing message as json
-
-    resolve.json({
-    description: 'ping',
-    message: 'pong',
-    date: new Date()});
+const mongoose = require('mongoose');
+const port = 3000 ;
+const userSchema = new mongoose.Schema({
+  username: String,
+  email: String,
 });
+
+const User = mongoose.model('User', userSchema);
+const connectionString = 'mongodb+srv://goru-2004:goru-2004@cluster.0cv8pyz.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
+  app.get('/', async (req, res) => {
+    try {
+      const users = await User.find().exec();
+      res.json(users);
+    } catch (error) {
+      console.error('Error retrieving user data:', error);
+    }
+  });
+
 
 if (require.main === module) {
   app.listen(port, () => {
